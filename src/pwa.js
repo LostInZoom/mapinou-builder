a71e857ab6ab249ed8516efd9fead03575a4d741
jberli <justinberli@gmail.com>
2025-01-17 15:06:03 +0100

    updated service workers

--- src/pwa.js
+++ src/pwa.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', init, false);
 function init() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register(new URL('./sw.js', import.meta.url))
       .then((reg) => {
-        console.log('Service worker registered -->', reg);
+        
       }, (err) => {
         console.error('Service worker not registered -->', err);
       });
